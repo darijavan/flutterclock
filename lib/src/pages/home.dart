3fe27e578af46c6571a5e3f8a5e54e46eab0b176
Randrianandraina Hajanirina Ridjvan <darijavan@gmail.com>
2021-04-11 00:35:58 +0300

    add contents to homepage

--- lib/src/pages/home.dart
+++ lib/src/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutterclock/src/constants/colors.dart';
 import 'package:flutterclock/src/widgets/clock.dart';
+import 'package:intl/intl.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -10,12 +11,113 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var formattedTime = DateFormat('HH:mm').format(now);
+    var formattedDate = DateFormat('EEE, d MMM').format(now);
+    var timezoneString = now.timeZoneOffset.toString().split('.').first;
+
     return Scaffold(
       backgroundColor: BACKGROUND_COLOR,
-      body: Center(
-        child: Clock(
-          smooth: true,
-          size: Size.fromRadius(150),
+      body: SafeArea(
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: () {},
+                  child: Column(
+                    children: [
+                      FlutterLogo(),
+                      Text(
+                        'Clock',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            VerticalDivider(
+              width: 1,
+              color: Colors.white54,
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Clock',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    Text(
+                      formattedTime,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 64,
+                      ),
+                    ),
+                    Text(
+                      formattedDate,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Clock(
+                      size: Size.fromRadius(100),
+                    ),
+                    SizedBox(
+                      height: 12,
+                    ),
+                    Text(
+                      'Timezone',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.language,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          'UTC${timezoneString.startsWith('-') ? '-' : '+'}$timezoneString',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
